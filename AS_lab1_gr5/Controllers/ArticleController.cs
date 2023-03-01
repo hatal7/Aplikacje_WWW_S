cb055ef8dbdad0361983bde9e8f8e8d677689b61
Dominik Dąbek <dd@dd.dd>
2023-03-01 15:51:18 +0100

    task number 22

---
+++ AS_lab1_gr5/Controllers/ArticleController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using AS_lab1_gr5;
+
+public class ArticleController : Controller
+{
+    public IActionResult Index()
+    {
+        var article = new Article
+        {
+            Id = 1,
+            Title = "Artykuł 1",
+            Content = "Lorem ipsum...",
+            CreationDate = DateTime.Now
+        };
+
+        return View(article);
+    }
+}
---
+++ AS_lab1_gr5/Models/Articles.cs
@@ -0,0 +1,12 @@
+namespace AS_lab1_gr5
+{
+    public class Article
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreationDate { get; set; }
+}
+
+}
+
