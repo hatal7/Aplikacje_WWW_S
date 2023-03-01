d72eafe4d9ca43125d88c7d43f6c4526dab56067
Adrian_Kwec <akk@gmail.com>
2023-03-01 15:58:45 +0100

    Lab 1dotnet run --project AS_lab1_gr1

---
+++ AS_lab1_gr1/Controllers/ArticleController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using AS_lab1_gr1.Models;
+namespace AS_lab1_gr1.Controllers;
+public class ArticleController:Controller
+{
+    public IActionResult Index()
+    {
+        var article = new Article
+        {
+            Id = 1,
+            Title = "Artykuł 1",
+            Content = "Lorem ipsum...",
+            CreationDate = DateTime.Now
+        };
+        return View(article);
+    }
+}
---
+++ AS_lab1_gr1/Models/Articles.cs
@@ -0,0 +1,8 @@
+namespace AS_lab1_gr1.Models;
+public class Article
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreationDate { get; set; }
+}
